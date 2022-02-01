a8f390d3af9c82410bdbeb4f52d95b46f45fafe2
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-02-01 12:39:06 +0000

    setup the project to use environment variables to capture the sitemap url

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const { generateReport } = require('./generate-report.js');
 const { generateLighthouseReports } = require('./lighthouse-scraper.js');
 
+// npm run report --url="https://www.domain.com/sitemap.xml"
+const siteMapUrl = process.env.npm_config_url;
+
 // TODO fix async logic. generateReport finished before generateLighthouseReports
 (async () => {
-    await generateLighthouseReports()
+    if (siteMapUrl) {
+        await generateLighthouseReports(siteMapUrl)
+    }
     await generateReport()
 })();
